3a697ff83967670b667b4417b9407fdda78f55c2
Joshua Anderson <j@joshua-anderson.com>
2019-04-04 13:09:54 -0700

    Booting into protected mode

--- Makefile
+++ Makefile
@@ -1,5 +1,35 @@
-run: sol.img
-	qemu-system-x86_64 -drive format=raw,file=sol.img
+ARCH ?= x86_64
 
-sol.img:
-	./mkimage.sh
+KERNEL := out/sol.bin
+IMG := out/sol.img
+ROOTFS := out/rootfs
+
+ASM_SRC := $(wildcard arch/$(ARCH)/*.asm)
+ASM_OBJ := $(patsubst arch/$(ARCH)/%.asm, \
+	out/arch/$(ARCH)/%.o, $(ASM_SRC))
+
+LD_SCRIPT := arch/$(ARCH)/linker.ld
+GRUB_CFG := arch/$(ARCH)/grub.cfg
+
+all: $(KERNEL)
+
+run: $(IMG)
+	qemu-system-x86_64 -drive format=raw,file=out/sol.img
+
+clean:
+	rm -rf out
+
+img: $(IMG)
+
+$(IMG): $(KERNEL) $(GRUB_CFG)
+	mkdir -p $(ROOTFS)/boot/grub2
+	cp $(GRUB_CFG) $(ROOTFS)/boot/grub2/
+	cp $(KERNEL) $(ROOTFS)/boot/
+	./mkimage.sh $(IMG) $(ROOTFS)
+
+$(KERNEL): $(ASM_OBJ) $(LD_SCRIPT)
+	ld -n -T $(LD_SCRIPT) -o $(KERNEL) $(ASM_OBJ)
+
+out/arch/$(ARCH)/%.o: arch/$(ARCH)/%.asm
+	@mkdir -p $(shell dirname $@)
+	nasm -felf64 $< -o $@
--- mkimage.sh
+++ mkimage.sh
@@ -1,17 +1,18 @@
 #!/bin/env bash
 
-dd if=/dev/zero of=sol.img bs=512 count=32768
-parted sol.img mklabel msdos # Add MBR
-parted sol.img mkpart primary fat32 2048s 30720s # Create OS parition
-parted sol.img set 1 boot on
+dd if=/dev/zero of=$1 bs=512 count=32768
+parted $1 mklabel msdos # Add MBR
+parted $1 mkpart primary fat32 2048s 30720s # Create OS parition
+parted $1 set 1 boot on
 LOOP0=$(sudo losetup -f)
-sudo losetup ${LOOP0} sol.img
+sudo losetup ${LOOP0} $1
 LOOP1=$(sudo losetup -f)
-sudo losetup ${LOOP1} sol.img -o 1048576
+sudo losetup ${LOOP1} $1 -o 1048576
 sudo mkdosfs -F32 -f 2 ${LOOP1}
 sudo mkdir /mnt/sol_grub
 sudo mount ${LOOP1} /mnt/sol_grub
-sudo grub2-install --target=i386-pc --boot-directory=/mnt/sol_grub --no-floppy --modules="normal part_msdos ext2 multiboot" ${LOOP0}
+sudo grub2-install --target=i386-pc --boot-directory=/mnt/sol_grub/boot --no-floppy --modules="normal part_msdos ext2 multiboot" ${LOOP0}
+sudo cp -r $2/* /mnt/sol_grub
 sudo umount /mnt/sol_grub
 sudo rm -rf /mnt/sol_grub
 sudo losetup -d ${LOOP0}
