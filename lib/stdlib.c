0b669229933eb371dbc41c8462f26955a54e585d
Joshua Anderson <j@joshua-anderson.com>
2019-04-09 09:11:03 -0700

    Adding more stdlib functions

--- lib/stdlib.c
+++ lib/stdlib.c
@@ -1,10 +1,28 @@
 #include "stdlib.h"
 
 void *memset(void *dst, int c, size_t n) {
-  int *tmp = (int *)dst;
+  char *tmp = (char *)dst;
   for (int i = 0; i < n; i++) {
-    tmp[i] = c;
+    tmp[i] = (char) c;
   }
 
   return dst;
+}
+
+void *memcpy(void *dst, const void *src, size_t n) {
+  for (int i = 0; i < n; i++) {
+    ((char *)dst)[i] = ((char *)src)[i];
+  }
+
+  return dst;
+}
+
+size_t strlen(const char *s) {
+  size_t len = 0;
+  while(*s != NULL) {
+    len++;
+    s += 1;
+  }
+
+  return len;
 }
--- lib/stdlib.h
+++ lib/stdlib.h
@@ -1,8 +1,11 @@
 #ifndef __STDLIB_H__
 #define __STDLIB_H__
 
+//TODO: Is this the right call?
 #define size_t int
-
+#define NULL 0
 void *memset(void *dst, int c, size_t n);
+void *memcpy(void *dst, const void *src, size_t n);
+size_t strlen(const char *s);
 
 #endif
